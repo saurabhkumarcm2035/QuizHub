c2882dfafb10a018240ae70c0a20435820c4eaa9
saurabhkumarcm2035 <saurabhyadavsy986@gmail.com>
2025-08-25 18:30:33 +0530

    Configure frontend for production

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a new axios instance with a base URL
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://quizhub-backend-g6ps.onrender.com/api',
 });
 
 /* Add a request interceptor. This function will run BEFORE every
